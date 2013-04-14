93417dff4bba665a9f2ee2cb170c5bdf8f82b48b
André Oriani <aoriani@gmail.com>
2013-04-14 18:31:36 -0300

    Implementing Saving State

--- TicTacToe/src/orion/tictactoe/Square.java
+++ TicTacToe/src/orion/tictactoe/Square.java
@@ -3,6 +3,8 @@ package orion.tictactoe;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.util.AttributeSet;
 import android.widget.ImageView;
 
@@ -11,7 +13,7 @@ public class Square extends ImageView {
 
     
     private static final int MAX_LINES = 3;
-    private static final int MAX_COLUMN = 3; 
+    private static final int MAX_COLUMNS = 3; 
 
     /** The column */
     private int mX = 0;
@@ -21,6 +23,46 @@ public class Square extends ImageView {
 
     /** The state of the Square - empty, cross or nought */
     private Piece mState = Piece.EMPTY;
+    
+    
+    private static class SquareSavedState extends BaseSavedState{
+         private int mState;
+   
+        public SquareSavedState(Parcelable superParcelable, Piece state) {
+            super(superParcelable);
+            mState = state.ordinal();
+        }
+
+        public SquareSavedState(Parcel parcel) {
+            super(parcel);
+            mState = parcel.readInt();
+        }
+        
+        public Piece getState(){
+            return Piece.values()[mState];
+        }
+
+        /* (non-Javadoc)
+         * @see android.view.AbsSavedState#writeToParcel(android.os.Parcel, int)
+         */
+        @Override
+        public void writeToParcel(Parcel dest, int flags) {
+            super.writeToParcel(dest, flags);
+            dest.writeInt(mState);
+        }
+
+        @SuppressWarnings("unused")
+        public static final Parcelable.Creator<SquareSavedState> CREATOR = new Parcelable.Creator<SquareSavedState>() {
+            public SquareSavedState createFromParcel(Parcel in) {
+                return new SquareSavedState(in);
+            }
+
+            public SquareSavedState[] newArray(int size) {
+                return new SquareSavedState[size];
+            }
+        };
+
+    }
     
     public Square(Context context) {
         super(context);
@@ -36,10 +78,10 @@ public class Square extends ImageView {
         TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.Square);
         try {
             int x = a.getInt(R.styleable.Square_x, 0);
-            if(x >= 0 && x < 3) mX = x;
+            if(x >= 0 && x < MAX_LINES) mX = x;
             
             int y = a.getInt(R.styleable.Square_y, 0);
-            if(y >= 0 &&  y < 3) mY = y;
+            if(y >= 0 &&  y < MAX_COLUMNS) mY = y;
             
             int state = a.getInt(R.styleable.Square_state, 0);
             mState = Piece.values()[state];
@@ -48,4 +90,26 @@ public class Square extends ImageView {
         }
     }
 
+    /* (non-Javadoc)
+     * @see android.view.View#onRestoreInstanceState(android.os.Parcelable)
+     */
+    @Override
+    protected void onRestoreInstanceState(Parcelable state) {
+        if(state instanceof SquareSavedState){
+            SquareSavedState savedState = (SquareSavedState) state;
+            super.onRestoreInstanceState(savedState.getSuperState());
+            mState = savedState.getState();
+        }else{
+            super.onRestoreInstanceState(state);
+        }
+    }
+
+    /* (non-Javadoc)
+     * @see android.view.View#onSaveInstanceState()
+     */
+    @Override
+    protected Parcelable onSaveInstanceState() {
+        Parcelable superParcelable = super.onSaveInstanceState();
+        return new SquareSavedState(superParcelable, mState);
+    }  
 }
